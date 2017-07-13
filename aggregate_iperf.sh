9448f4086a8bf5bbc11d888613a6f08fabbecaf5
Emiapwil (Gao Kai) <emiapwil@gmail.com>
2017-07-13 15:16:13 +0800

    Add gap to the aggregated data
    Signed-off-by: Emiapwil (Gao Kai) <emiapwil@gmail.com>

--- aggregate_iperf.sh
+++ aggregate_iperf.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 N=$1
+GAP=$2
 
 rm -rf iperf.log
 for i in $(seq 0 $N); do
     cat $i.log | sed '1,3d;s/-/ /g;/ID/q' | sed 'N;$d' \
-        | awk '{print '$i'+2 " " $4 " " $11}' >> iperf.log
+        | awk '{print '$i'+2 " " $4+'$GAP'*'$i' " " $11}' >> iperf.log
 done
