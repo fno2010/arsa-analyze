1e2587fb38cd5f0471e632f6284c13733a6f7b05
jensenzhang <hack@jensen-zhang.site>
2018-07-28 14:13:47 +0800

    Redraw eval figures
    Signed-off-by: jensenzhang <hack@jensen-zhang.site>

--- plottt.py
+++ plottt.py
@@ -2,27 +2,38 @@
 
 import matplotlib.pyplot as plot
 import numpy as np
-import math
 from statistics import mean, stdev
 
 if __name__ == '__main__':
     N = 10
     T = {}
-    with open('test-time.log', 'r') as f:
-        t0 = list(map(float, f.readlines()))
-        t = sorted(t0)
+    t = []
+    for i in range(N):
+        with open('test-time-%d.log' % i, 'r') as f:
+            t0 = list(map(float, f.readlines()))
+            t += t0
 
-    x = range(len(t))
-    c = np.cumsum([1.0 / len(t)] * len(t))
+        for j in range(len(t0)):
+            T[j] = (T[j] if j in T else []) + [t0[j]]
+
+    mt, sd = [], []
+    for i in sorted(T.keys()):
+        mt += [mean(T[i])]
+        sd += [stdev(T[i])]
 
     fig, ax = plot.subplots()
-    ax.plot(list(range(len(t0))), t0)
+    ax.errorbar(list(range(len(T))), mt, sd)
 
     plot.xlabel('Samples', fontsize=14)
     plot.ylabel('Prediction Time (s)')
 
     fig.savefig('pred-time.pdf')
 
+    t = sorted(t)
+    x = range(len(t))
+    c = np.cumsum([1.0 / len(t)] * len(t))
+
+    print(t)
     fig, ax = plot.subplots()
     ax.plot(t, c)
 
--- test-time.sh
+++ test-time.sh
@@ -4,12 +4,12 @@ N=$(python testgen.py)
 
 echo $N > test-time.meta
 
-python simprediction.py 32 $N | awk -F: -e '{}{print $3}{}' > test-time.log
+# python simprediction.py 32 $N | awk -F: -e '{}{print $3}{}' > test-time.log
 
-#for i in $(seq 1 10)
-#do
-#    echo test-%i.log
-#    python simprediction.py 32 $N | awk -F: -e '{}{print $3}{}' > time-%i.log
-#done
+for i in $(seq 0 9)
+do
+    echo test-time-$i.log
+    python simprediction.py 32 $N | awk -F: -e '{}{print $3}{}' > test-time-$i.log
+done
 
 python plottt.py
